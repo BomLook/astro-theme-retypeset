aca9ee209a606d49d092f03b5b0c795565e9205b
BomLook <yangrenbing7@gmail.com>
2026-03-01 13:25:40 +0800

    简单修改了下基础信息

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const themeConfig: ThemeConfig = {
     // use i18n title/subtitle/description from src/i18n/ui.ts instead of static ones above
     i18nTitle: true, // true | false
     // author name
-    author: 'radishzz',
+    author: 'BomLook',
     // site url
     url: 'https://retypeset.radishzz.cc',
     // base path
@@ -27,7 +27,7 @@ export const themeConfig: ThemeConfig = {
   // COLOR SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START
   color: {
     // default theme mode
-    mode: 'light', // light | dark | auto
+    mode: 'dark', // light | dark | auto
     light: {
       // primary color
       // used for title, hover, etc
